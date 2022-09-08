72197925f0e4d57b078be9ad69fcedf03941488e
agustinadisiot <agustinadisiot@Agustinas-MacBook-Pro.local>
2022-09-08 15:32:04 -0300

    Adds function for circle area

--- math.go
+++ math.go
@@ -1,9 +1,15 @@
 package math
 
+import "math"
+
 func Add(x, y int) (res int) {
 	return x + y
 }
 
 func Subtract(x, y int) (res int) {
 	return x - y
+}
+
+func CircleArea(r float64) (res float64) {
+	return math.Pi * r * r
 }
